a794dc010a0e15e908316c3066da2bba21ce52e8
Simon Johansson <simjohansson@gmail.com>
2021-04-14 20:45:58 +0200

    no moongoose

--- src/app.ts
+++ src/app.ts
@@ -35,12 +35,12 @@ const app = express();
 const mongoUrl = MONGODB_URI;
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-    // process.exit();
-});
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(
+//     () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+// ).catch(err => {
+//     console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+//     // process.exit();
+// });
 
 // Express configuration
 app.use(session({
