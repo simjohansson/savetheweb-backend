d2b7058e4b67efba26e76824f8bd4829ec019ae0
Simon Johansson <simjohansson@gmail.com>
2021-04-15 20:45:47 +0200

    Get top 5 highscore

--- src/controllers/highscore.ts
+++ src/controllers/highscore.ts
@@ -3,22 +3,30 @@ import { HighscoreDocument, HighscoreModel } from "../models/Highscore";
 import { check, validationResult } from "express-validator";
 
 export const getHighscore = async (req: Request, res: Response) => {
-    const result  = await HighscoreModel.find({});
-    res.json(result);    
+  const result = await getTop5Score();
+  res.json(result);
 };
 
 export const tryUpdateHighscore = async (req: Request, res: Response) => {
-    await check("name", "Name cannot be blank").not().isEmpty().run(req);
-    await check("score", "Score is not valid").isInt().run(req);
+  await check("name", "Name cannot be blank").not().isEmpty().run(req);
+  await check("score", "Score is not valid").isInt().run(req);
 
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      //  req.flash("errors", errors.array());
-      return  res.status(400).json(errors.array());
-    }
-    const data = req.body as HighscoreDocument;
-    const highscore = new HighscoreModel({name: data.name, score: data.score});
+  if (!errors.isEmpty()) {
+    //  req.flash("errors", errors.array());
+    return res.status(400).json(errors.array());
+  }
+  const data = req.body as HighscoreDocument;
+  const result = await getTop5Score();
+  if (result.find(x => x.score < data.score)) {
+    const highscore = new HighscoreModel({ name: data.name, score: data.score });
     highscore.save((error) => console.log(error));
-    res.sendStatus(200);
+  }
+  res.sendStatus(200);
+};
+
+
+const getTop5Score = async () => {
+  return await HighscoreModel.find({}).sort({ score: -1 }).limit(5);
 };
